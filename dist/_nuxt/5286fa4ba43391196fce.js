2170339b91521cc3890ec06c039ff0ecc905b492
baldychristophe <baldychristophe@gmail.com>
2019-08-27 17:05:31 +0200

    update dist

---
+++ dist/_nuxt/5286fa4ba43391196fce.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{145:function(t,n,e){"use strict";e.r(n);var l=e(22),component=Object(l.a)({},function(){var t=this.$createElement;return(this._self._c||t)("div",[this._v("Hello world with nuxt!")])},[],!1,null,null,null);n.default=component.exports}}]);
