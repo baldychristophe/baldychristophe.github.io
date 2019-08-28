c5f2ed332951b2b1f2277f1321b418b83d7fd12a
baldychristophe <baldychristophe@gmail.com>
2019-08-28 14:35:39 +0200

    client/pages: add head in html page

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -4,7 +4,17 @@
 
 <script>
 export default {
-  
+  head () {
+    return {
+      title: 'Christophe Baldy - Resume',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: 'Christophe Baldy resume' },
+        { hid: 'keywords', name: 'keywords', content: 'development, software, Christophe Baldy, resume' },
+      ],
+    }
+  } 
 }
 </script>
 
